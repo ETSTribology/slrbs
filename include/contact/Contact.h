49f44fe0546585c846d77d205ab8ac78b9fd6195
Antoine Boucher <antoine.bou13@gmail.com>
2025-04-06 19:06:43 -0400

    Update Contact.h

--- include/contact/Contact.h
+++ include/contact/Contact.h
@@ -1,7 +1,5 @@
 #pragma once
-
 #include "joint/Joint.h"
-
 #include <Eigen/Dense>
 
 class RigidBody;
@@ -18,31 +16,25 @@ class Contact : public Joint
 {
 public: 
     static float mu;            // Coefficient of friction (global)
-
 public:
-
     // Constructor with all parameters.
-    Contact(RigidBody* _body0, RigidBody* _body1, const Eigen::Vector3f& _p, const Eigen::Vector3f& _n, float _phi);
-
+    Contact(RigidBody* *body0, RigidBody* *body1, const Eigen::Vector3f& *p, const Eigen::Vector3f& *n, float _phi);
     virtual ~Contact();
-
     virtual eConstraintType getType() const override { return kContact; }
-
     Eigen::Vector3f p;          // The contact point.
     Eigen::Vector3f n;          // The contact normal.
     Eigen::Vector3f t, b;       // Tangent directions.
     float pene;                 // Penetration
-
-    virtual void computeJacobian();
-
+    
+    // Fixed: Added 'override' keyword to match the virtual function in Joint
+    virtual void computeJacobian() override;
+    
     // Computes a contact frame using the contact normal @a n
     // and the provided direction @a dir, which is aligned with the first tangent direction.
     // The resulting frame is stored in the basis vectors @a n, @a t1, and @a t2.
     //
     void computeContactFrame();
-
 protected:
-
     // Default constructor (hidden)
     explicit Contact();
 };
